6958c141f7fc82ea414dc0d6ba77784f7a778762
Alok Mishra <alok@alokmishra.com>
2015-02-12 14:35:29 -0500

    added metadata

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # Userscripts
-
 A collection of personal userscripts to be used with Greasemonkey, Tampermoney, etc.
 
 ## Zoho Invoice Notes
-
 Zoho invoice is a great free alternative to keep track of clients, but I want to be able to see the notes for each line item instead of haivng to hover of the Notes icon.
 This script displays the notes entered for each task right beside it, so you can tell what that line item was for in one glance.
--- zoho_invoice_notes.user.js
+++ zoho_invoice_notes.user.js
@@ -1,11 +1,15 @@
 // ==UserScript==
 // @name        Invoice Notes
-
+// @namespace   https://github.com/alok-mishra/userscripts
 // @description Shows invoice notes without having to hover your mouse
-// @include     https://invoice.zoho.com/*
-// @version     1
+// @author      Alok Mishra <alok@alokmishra.com>
+// @match       https://invoice.zoho.com/*
+// @version     0.2
+// @downloadURL https://github.com/alok-mishra/userscripts/raw/master/zoho_invoice_notes.user.js
+// @updateURL   https://github.com/alok-mishra/userscripts/raw/master/zoho_invoice_notes.user.js
 // @grant       none
 // @require     http://code.jquery.com/jquery-2.1.3.min.js
+// @icon        http://i61.tinypic.com/20af9dy.png
 // ==/UserScript==
 
 $(function() {

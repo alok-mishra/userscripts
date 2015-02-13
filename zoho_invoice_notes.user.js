fa63347a254c057479fcac2dc83b3de294ce8bac
Alok Mishra <alok@alokmishra.com>
2015-02-13 11:26:13 -0500

    icon change, notes width

--- zoho_invoice_notes.user.js
+++ zoho_invoice_notes.user.js
@@ -4,12 +4,12 @@
 // @description Shows invoice notes without having to hover your mouse
 // @author      Alok Mishra <alok@alokmishra.com>
 // @match       https://invoice.zoho.com/*
-// @version     0.2
+// @version     0.4
 // @downloadURL https://github.com/alok-mishra/userscripts/raw/master/zoho_invoice_notes.user.js
 // @updateURL   https://github.com/alok-mishra/userscripts/raw/master/zoho_invoice_notes.user.js
 // @grant       none
 // @require     http://code.jquery.com/jquery-2.1.3.min.js
-// @icon        http://i61.tinypic.com/20af9dy.png
+// @icon        http://i57.tinypic.com/2exy0ra.png
 // ==/UserScript==
 
 $(function() {
@@ -32,8 +32,8 @@ function showNotes() {
 
             setTimeout(function() {
                 // console.log ($('.tooltip-inner:first').text());
-                $(v).siblings('.my-notes').remove();
-                $(v).parent().css('width', '300px').append('<span class="my-notes">' + $('.tooltip-inner:first').text() + '</span>');
+                $(v).siblings('div').remove();
+                $(v).parent().append($('.tooltip-inner:first div').css('width', '400px'));
                 $(v).mouseout();
             }, (i+1)*200);
 
